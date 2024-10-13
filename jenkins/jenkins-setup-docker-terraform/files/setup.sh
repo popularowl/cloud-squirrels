7f1f6db2eddb114563c749e15e0a50a26124d981
Saulius Zukauskas <saulius.zukauskas@gmail.com>
2024-10-13 02:10:13 +0100

    bump up to jenkins:2.480

--- jenkins/jenkins-setup-docker-terraform/files/setup.sh
+++ jenkins/jenkins-setup-docker-terraform/files/setup.sh
@@ -9,3 +9,27 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt -y install docker-ce &&
+
+# build jenkins container image with default admin user
+cd /tmp && docker build -t popularowl/jenkins .
+
+# run newly built jenkins container on port 8080
+docker run -d --name jenkins-server -p 8080:8080 popularowl/jenkins
+
+# install remaining dependencies
+apt -y install nginx
+apt -y install ufw
+
+# setup debian firewall
+ufw status verbose
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw allow 22
+ufw allow 80
+yes | ufw enable
+
+# update nginx configuration
+rm -f /etc/nginx/sites-enabled/default
+cp -f /tmp/jenkins-proxy /etc/nginx/sites-enabled
+service nginx restart
--- jenkins/jenkins-setup-docker-terraform/main.tf
+++ jenkins/jenkins-setup-docker-terraform/main.tf
@@ -62,45 +62,6 @@ resource "digitalocean_droplet" "jenkinsci-server" {
             "/tmp/setup.sh"
         ]
     }
-
-    // run all necessary commands via remote shell 
-    provisioner "remote-exec" {
-        inline = [
-
-            # steps to setup docker ce
-            "apt update",
-            "apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common",
-            "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -",
-            "add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable\" ",
-            "apt update",
-            "apt-cache policy docker-ce",
-            "apt -y install docker-ce",
-
-            # build jenkins image with default admin user
-            "cd /tmp && docker build -t popularowl/jenkins .",
-
-            # run newly built jenkins container on port 8080
-            "docker run -d --name jenkins-server -p 8080:8080 popularowl/jenkins",
-
-            # install remaining dependencies
-            "apt -y install nginx",
-            "apt -y install ufw",
-            
-            # setup debian firewall
-            "ufw status verbose",
-            "ufw default deny incoming",
-            "ufw default allow outgoing",
-            "ufw allow ssh",
-            "ufw allow 22",
-            "ufw allow 80",
-            "yes | ufw enable",
-            
-            # update nginx configuration
-            "rm -f /etc/nginx/sites-enabled/default",
-            "cp -f /tmp/jenkins-proxy /etc/nginx/sites-enabled",
-            "service nginx restart"        
-        ]
-    }
 }
 
 # print out ip address of created Jenkins server VM
